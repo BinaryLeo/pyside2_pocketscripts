6a4a93ba47514094bd53a3aeab447694c2656392
Binary Leo <leomouracss@fastmail.com>
2022-02-03 13:33:49 -0300

    pystwo_003.py
    pystwo_003.py file

---
+++ Basic/pystwo_003.py
@@ -0,0 +1,13 @@
+# pip3 install pyside2
+
+from PySide2.QtWidgets import QApplication, QPushButton
+# The handler (QApplication) and a basic empty GUI widget with a single 
+# push-able button (QPushButton)
+
+import sys # Only needed for access to command line arguments 
+
+app = QApplication(sys.argv)
+#We neeed one (only one) instance per application
+window = QPushButton('Push me plz') # create a new window (A QWidget)
+window.show() # Windows are hidden by default
+app.exec_()# Start the event loop
